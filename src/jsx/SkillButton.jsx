c57950d5a9dba7e363287a9fd716da5c07dcd9c0
José Antonio Vázquez <javazquez@uc-3-337.AluVedruna.local>
2024-11-22 17:30:03 +0100

    anadido tarjetas de skills

--- src/jsx/SkillButton.jsx
+++ src/jsx/SkillButton.jsx
@@ -16,16 +16,16 @@ const SkillButton = ({ initialSkills }) => {
     <div>
       <div className="flex flex-wrap justify-center gap-8">
         {skills.map((skill, index) => (
-          <div key={index} className="text-center">
-            <img src={skill.image} alt={skill.name} className="w-10 h-10 rounded-full" />
-            <p>{skill.name}</p>
+          <div key={index} className="bg-white shadow-lg rounded-lg p-6 w-48 text-center">
+            <img src={skill.image} alt={skill.name} className="w-16 h-16 rounded-full mx-auto mb-4" />
+            <p className="text-lg font-semibold">{skill.name}</p>
           </div>
         ))}
       </div>
       <div className="flex justify-center mt-6">
         <button
           className="bg-blue-500 text-white p-3 rounded-full hover:bg-blue-600 transition duration-300"
-          onClick={changeSkills} // Asegúrate de que este onClick esté correctamente asignado
+          onClick={changeSkills} 
         >
           <svg className="w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
             <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l7-7-7-7" />
